2280fa1a8c10adce3f8a94eda7f78576bdfa2220
Rodrigo Santos <rodrigo.santos@talktelecom.com.br>
2018-05-04 15:07:38 -0300

    Transforming output to umd

--- source/index.js
+++ source/index.js
@@ -1,20 +1,23 @@
-export function validate(validation, model)
-{
-    const propertyErrorCollection = Object.keys(validation)
-        .map(property => validateProperty(validation, property, model))
-        .filter(property => property[Object.keys(property)[0]].length)
-    return propertyErrorCollection.length? Object.assign(...propertyErrorCollection) : undefined
+import * as Constraint from "./constraint";
+import * as CommonValidation from "./common-validation";
+
+export function validate(validation, model) {
+  const propertyErrorCollection = Object.keys(validation)
+    .map(property => validateProperty(validation, property, model))
+    .filter(property => property[Object.keys(property)[0]].length);
+  return propertyErrorCollection.length
+    ? Object.assign(...propertyErrorCollection)
+    : undefined;
 }
 
-function validateProperty(validation, property, model)
-{
-    const propertyValidationCollection = validation[property];
-    const value = model[property];
-    const validationResult = new Object();
-    validationResult[property] = propertyValidationCollection
-        .map(propertyValidation => propertyValidation(value))
-        .filter(error => error != undefined)
-    return validationResult;
+function validateProperty(validation, property, model) {
+  const propertyValidationCollection = validation[property];
+  const value = model[property];
+  const validationResult = new Object();
+  validationResult[property] = propertyValidationCollection
+    .map(propertyValidation => propertyValidation(value))
+    .filter(error => error != undefined);
+  return validationResult;
 }
 
 export * from "./constraint";
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: './source/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js'
-    }    
-}
+  entry: "./source/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    library: "kaizen-validation",
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015"]
+        }
+      }
+    ]
+  }
+};
